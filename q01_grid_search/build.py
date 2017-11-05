8a51b020945e8ec9ab277732fdd631c20f5a08bb
jyotika03 <jyotika.tewari1992@yahoo.com>
2017-11-05 18:43:25 +0000

    Done

--- q01_grid_search/build.py
+++ q01_grid_search/build.py
@@ -1,3 +1,4 @@
+# %load q01_grid_search/build.py
 # Default imports
 
 import warnings
@@ -6,6 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
+import numpy as np
 
 loan_data = pd.read_csv('data/loan_prediction.csv')
 X_bal = loan_data.iloc[:, :-1]
@@ -17,8 +19,23 @@ param_grid = {"max_features": ['sqrt', 4, "log2"],
               "max_depth": [40, 20, 10],
               "max_leaf_nodes": [5, 10, 2]}
 
-
+model=RandomForestClassifier(oob_score =True,random_state=9)
 # Write your solution here :
+def grid_search(X_train, y_train,model,param_grid,cv=3):
+    modelsearch=GridSearchCV(estimator=model,param_grid=param_grid,cv=cv)
+    modelsearch.fit(X_train,y_train)
+    modelsearch.transform(X_train)
 
+    #bstf=modelsearch.best_params_
+    #print bstf
 
+    cvr_score=modelsearch.cv_results_['mean_test_score']
+    #print cvr_score
+    #print type(cvr_score)
+    cvr_params=modelsearch.cv_results_['params']
+    #print cvr_params
+    #print type(cvr_params)
 
+    #print np.shape(cvr_score)
+    #print np.shape(cvr_params)
+    return modelsearch,cvr_params,cvr_score
