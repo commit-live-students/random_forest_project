358600bf39794967df54a5fdd2d6272c8377ac5d
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-11-13 02:59:40 +0000

    Done

--- q01_grid_search/build.py
+++ q01_grid_search/build.py
@@ -16,9 +16,14 @@ param_grid = {"max_features": ['sqrt', 4, "log2"],
               "n_estimators": [10, 50, 120],
               "max_depth": [40, 20, 10],
               "max_leaf_nodes": [5, 10, 2]}
+model = RandomForestClassifier(random_state=9,oob_score = True)
+def grid_search(X_train,y_train,model,param_grid,cv=3):
+    grid = GridSearchCV(estimator=model,param_grid=param_grid)
+    grid = grid.fit(X_train,y_train)
+    l1 = grid.cv_results_["params"]
 
 
-# Write your solution here :
 
-
-
+    l2 = grid.cv_results_["mean_train_score"]
+    return model,l1,l2
+    
