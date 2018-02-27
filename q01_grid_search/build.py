bd299f6c629bd8c557c357463886d19fc459ab5e
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-27 14:10:03 +0000

    Done

--- q01_grid_search/build.py
+++ q01_grid_search/build.py
@@ -1,3 +1,4 @@
+# %load q01_grid_search/build.py
 # Default imports
 
 import warnings
@@ -6,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+import numpy as np
 
 loan_data = pd.read_csv('data/loan_prediction.csv')
 X_bal = loan_data.iloc[:, :-1]
@@ -16,9 +19,14 @@ param_grid = {"max_features": ['sqrt', 4, "log2"],
               "n_estimators": [10, 50, 120],
               "max_depth": [40, 20, 10],
               "max_leaf_nodes": [5, 10, 2]}
+classifier = RandomForestClassifier(random_state=9,oob_score=True)
+
+def grid_search(X_train, y_train, classifier,param_grid,cv=3):
 
 
-# Write your solution here :
+    grid_search = GridSearchCV(estimator=classifier, param_grid=param_grid,cv=3)
+    grid_search.fit(X_train,y_train)
+    y_prediction = grid_search.predict(X_test)
+    return grid_search, tuple(grid_search.cv_results_['params']), grid_search.cv_results_['mean_test_score']
 
-
-
+#grid_search(X_train, y_train, classifier, param_grid)
