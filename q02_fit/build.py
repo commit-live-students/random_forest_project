b8511c98098d0bfeeed0b3d4807a0e8c6c00f4cd
dharmil25 <dharmil2524_shah@ymail.com>
2018-04-23 10:53:59 +0000

    Done

--- q02_fit/build.py
+++ q02_fit/build.py
@@ -22,6 +22,8 @@ grid, grid_param, grid_score = grid_search(X_train, y_train, rfc, param_grid, cv
 
 
 # Write your solution here :
-
-
-
+def fit(X_test, y_test):
+    model = grid.best_estimator_
+    model.fit(X_train,y_train)
+    y_pred = grid.predict(X_test)
+    return confusion_matrix(y_test,y_pred), classification_report(y_test,y_pred) , accuracy_score(y_test, y_pred)
