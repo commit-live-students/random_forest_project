cb281924db813bae3cc0fae98e7af16be1152986
abkedar <anupbhande@gmail.com>
2018-09-05 08:41:32 +0000

    Done

--- q01_grid_search/build.py
+++ q01_grid_search/build.py
@@ -1,7 +1,7 @@
 # Default imports
 
 import warnings
-warnings.filterwarnings("ignore")
+warnings.filterwarnings('ignore')
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -12,17 +12,19 @@ X_bal = loan_data.iloc[:, :-1]
 y_bal = loan_data.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X_bal, y_bal, test_size=0.33, random_state=9)
-param_grid = {"max_features": ['sqrt', 4, "log2"],
-              "n_estimators": [10, 50, 120],
-              "max_depth": [40, 20, 10],
-              "max_leaf_nodes": [5, 10, 2]}
+param_grid = {'max_features': ['sqrt', 4, 'log2'],
+              'n_estimators': [10, 50, 120],
+              'max_depth': [40, 20, 10],
+              'max_leaf_nodes': [5, 10, 2]}
 clf = RandomForestClassifier(oob_score=True, random_state=9)
 
 # Write your solution here :
-def grid_search(X_train, y_train, clf, param_grid, CV):
-    grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=CV)
+def grid_search(X_train, y_train, clf, param_grid, cv=3):
+    grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=cv)
     #optimized_grid = model(clf, cv_params = param_grid, cv = CV)
     grid_search.fit(X_train, y_train)
-    return grid_search, (grid_search.cv_results_['params']), (grid_search.cv_results_['mean_test_score'])
+    return (grid_search, (grid_search.cv_results_['params']), (grid_search.cv_results_['mean_test_score']))
+    
+grid_search(X_train, y_train, clf, param_grid=param_grid, cv)
 
-grid_search(X_train, y_train, clf, param_grid=param_grid, CV=3)
+
