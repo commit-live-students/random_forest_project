c01d12c97cf501935d052e3541d15a40235a354b
rushabh-mehta <rushabh123@gmail.com>
2018-12-29 16:43:53 +0000

    Done

--- q02_fit/build.py
+++ q02_fit/build.py
@@ -1,3 +1,4 @@
+# %load q02_fit/build.py
 # Default imports
 
 import pandas as pd
@@ -13,15 +14,18 @@ y_bal = loan_data.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X_bal, y_bal, test_size=0.33, random_state=9)
 rfc = RandomForestClassifier(oob_score=True, random_state=9)
-param_grid = {"max_features": ['sqrt', 4, "log2"],
-              "n_estimators": [10, 50, 120],
-              "max_depth": [40, 20, 10],
-              "max_leaf_nodes": [5, 10, 2]}
+param_grid = {'max_features': ['sqrt', 4, 'log2'],
+              'n_estimators': [10, 50, 120],
+              'max_depth': [40, 20, 10],
+              'max_leaf_nodes': [5, 10, 2]}
 
 grid, grid_param, grid_score = grid_search(X_train, y_train, rfc, param_grid, cv=3)
 
 
 # Write your solution here :
-
+def fit(X_test,y_test):
+    y_pred = grid.predict(X_test)
+    return confusion_matrix(y_test,y_pred),classification_report(y_test,y_pred),accuracy_score(y_test,y_pred)
+fit(X_test,y_test)
 
 
